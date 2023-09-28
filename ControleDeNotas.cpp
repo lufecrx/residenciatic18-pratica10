451d5fc0b0584ebd77c8b55617c0bfb320d4030f
Luiz Felipe <luizfelipecrx@gmail.com>
2023-09-27 22:54:56 -0300

    Adiciona funcionalidade para exibir boletim dos alunos

--- ControleDeNotas.cpp
+++ ControleDeNotas.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
+
+void exibirBoletins(const vector<string> &nomes, const vector<float> &notas1, const vector<float> &notas2)
+{
+    cout << "===============================" << endl;
+    cout << "      Boletim dos Alunos      " << endl;
+    cout << "===============================" << endl;
+
+    cout << "Nome\t\tNota 1\tNota 2\tMédia\tSituação" << endl;
+
+    for (size_t i = 0; i < nomes.size(); i++)
+    {
+        float media = (notas1[i] + notas2[i]) / 2;
+        string situacao = (media >= 7) ? "Aprovado" : "Reprovado";
+
+        cout << nomes[i] << "\t\t" << fixed << setprecision(1) << notas1[i] << "\t" << notas2[i] << "\t" << media << "\t" << situacao << endl;
+    }
+
+    cout << "===============================" << endl;
+    cout << endl;
+}
 
 int main()
 {
@@ -31,6 +52,7 @@ int main()
         notas2.push_back(n2);
     }
 
-    return 0;
+    exibirBoletins(nomes, notas1, notas2);
 
+    return 0;
 }
